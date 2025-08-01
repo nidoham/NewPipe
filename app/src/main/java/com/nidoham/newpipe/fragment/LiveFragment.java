e9bfe3620f8e2d0880190bf0970b6ec1e81a2ad5
nidoham <nidohamondol.official@gmail.com>
2025-08-01 14:08:01 +0600

    Added new uiux features

---
+++ app/src/main/java/com/nidoham/newpipe/fragment/LiveFragment.java
@@ -0,0 +1,33 @@
+package com.nidoham.newpipe.fragment;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import org.schabi.newpipe.databinding.ActivitySplashBinding;
+
+public class LiveFragment extends Fragment {
+        private ActivitySplashBinding binding;
+        
+        @Nullable
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            binding = ActivitySplashBinding.inflate(inflater, container, false);
+            return binding.getRoot();
+        }
+        
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            // Initialize live content here
+        }
+        
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            binding = null;
+        }
+}
--- app/src/main/java/com/nidoham/newpipe/fragments/HomeFragment.java
+++ app/src/main/java/com/nidoham/newpipe/fragments/HomeFragment.java
@@ -7,22 +7,83 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-import org.schabi.newpipe.R;
+import com.nidoham.newpipe.fragment.TrendingFragment;
+import com.nidoham.newpipe.fragment.LiveFragment;
+import org.schabi.newpipe.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    private FragmentHomeBinding binding;
+    private HomeTabAdapter tabAdapter;
     
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, 
                            @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_splash, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
     
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Initialize views and setup here
+        
+        setupViewPager();
+        setupTabLayout();
+    }
+    
+    private void setupViewPager() {
+        tabAdapter = new HomeTabAdapter(getActivity());
+        binding.viewPager.setAdapter(tabAdapter);
+        binding.viewPager.setOffscreenPageLimit(1); // Keep both fragments in memory
+        binding.viewPager.setUserInputEnabled(false);
+    }
+    
+    private void setupTabLayout() {
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Trending");
+                    break;
+                case 1:
+                    tab.setText("Live");
+                    break;
+            }
+        }).attach();
+    }
+    
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+    
+    // ViewPager2 Adapter for tabs
+    private static class HomeTabAdapter extends FragmentStateAdapter {
+        
+        public HomeTabAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+        
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return new TrendingFragment();
+                case 1:
+                    return new LiveFragment();
+                default:
+                    return new TrendingFragment();
+            }
+        }
+        
+        @Override
+        public int getItemCount() {
+            return 2; // Trending and Live tabs
+        }
     }
 }
